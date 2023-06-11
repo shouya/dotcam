a4f1c9133da8bbe1b6601418f048aa87a45e1ba5
Shou Ya <shouya@users.noreply.github.com>
2023-06-11 20:59:15 +0900

    show camera feed

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
+use std::io::Read;
+
 use bevy::{
   prelude::{
-    default, App, DefaultPlugins, Image, NonSendMut, PluginGroup, ResMut,
-    Resource, World,
+    default, App, Assets, DefaultPlugins, Handle, Image, NonSendMut,
+    PluginGroup, Reflect, ReflectResource, ResMut, Resource, World,
   },
   render::render_resource::{Extent3d, TextureDimension, TextureFormat},
   window::{Window, WindowPlugin},
 };
-use bevy_inspector_egui::quick::ResourceInspectorPlugin;
-use bevy_reflect::Reflect;
-use image::codecs::jpeg::JpegDecoder;
-use nokhwa::{
-  pixel_format::{LumaFormat, RgbFormat},
-  Camera,
-};
+use bevy_egui::{EguiContexts, EguiPlugin};
+use image::{codecs::jpeg::JpegDecoder, ImageDecoder};
+use itertools::Itertools;
+use nokhwa::{pixel_format::LumaFormat, Camera};
 
 #[derive(Resource, Reflect, Default)]
+#[reflect(Resource)]
 struct CameraBuffer {
-  image: Image,
+  image: Handle<Image>,
   width: u32,
   height: u32,
 }
@@ -24,7 +24,7 @@ struct CameraBuffer {
 fn main() {
   let plugins = DefaultPlugins.set(WindowPlugin {
     primary_window: Some(Window {
-      title: "Window".into(),
+      title: "Dotcam".into(),
       resolution: (500., 500.).into(),
       ..default()
     }),
@@ -33,9 +33,11 @@ fn main() {
 
   App::new()
     .add_plugins(plugins)
+    .add_plugin(EguiPlugin)
+    .register_type::<CameraBuffer>()
     .add_startup_system(setup_camera)
     .add_system(update_camera_output)
-    .add_plugin(ResourceInspectorPlugin::<CameraBuffer>::default())
+    .add_system(inspect_buffer)
     .run();
 }
 
@@ -59,8 +61,12 @@ fn setup_camera(world: &mut World) {
     depth_or_array_layers: 1,
   };
   let dimension = TextureDimension::D2;
-  let format = TextureFormat::R8Unorm;
-  let image = Image::new_fill(extent, dimension, &[0], format);
+  let format = TextureFormat::Rgba8UnormSrgb;
+  let image = Image::new_fill(extent, dimension, &[100, 0, 0, 255], format);
+
+  let mut images = world.get_resource_mut::<Assets<Image>>().unwrap();
+
+  let image = images.add(image);
 
   world.insert_resource(CameraBuffer {
     image,
@@ -72,6 +78,7 @@ fn setup_camera(world: &mut World) {
 fn update_camera_output(
   mut camera: Option<NonSendMut<Camera>>,
   mut buffer: Option<ResMut<CameraBuffer>>,
+  mut images: ResMut<Assets<Image>>,
 ) {
   use nokhwa::utils::FrameFormat::MJPEG;
 
@@ -80,24 +87,53 @@ fn update_camera_output(
     None => return,
   };
   let image = match buffer.as_mut() {
-    Some(buffer) => &mut buffer.image,
+    Some(buffer) => images.get_mut(&buffer.image).unwrap(),
     None => return,
   };
 
   let frame = camera.frame().unwrap();
   debug_assert!(frame.source_frame_format() == MJPEG);
 
-  let mut decoder = JpegDecoder::new(&frame.buffer_bytes()).unwrap();
+  let decoder = JpegDecoder::new(frame.buffer()).unwrap();
 
-  debug_assert!(decoder.colortype() == image::ColorType: Rgb8);
+  debug_assert!(decoder.color_type() == image::ColorType::Rgb8);
 
   decoder
     .into_reader()
+    .unwrap()
     .bytes()
-    .chunks(3)
-    .zip(image.data.iter_mut())
-    .for_each(|(rgb, luma)| {
-      let rgb = rgb.unwrap();
-      *luma = rgb[0];
+    .tuples()
+    .zip(image.data.iter_mut().tuples())
+    .for_each(|((r, g, b), (dr, dg, db, _da))| {
+      *dr = r.unwrap();
+      *dg = g.unwrap();
+      *db = b.unwrap();
     });
+}
+
+fn inspect_buffer(
+  mut ctx: EguiContexts,
+  mut buffer: Option<ResMut<CameraBuffer>>,
+) {
+  let buffer = match buffer.as_mut() {
+    Some(buffer) => buffer,
+    None => return,
+  };
+
+  const SCALE: f32 = 0.1;
+
+  let texture_id = ctx
+    .image_id(&buffer.image)
+    .unwrap_or_else(|| ctx.add_image(buffer.image.clone()));
+
+  bevy_inspector_egui::egui::Window::new("Camera Buffer").show(
+    ctx.ctx_mut(),
+    |ui| {
+      ui.label(format!("Image: {}x{}", buffer.width, buffer.height));
+      ui.image(
+        texture_id,
+        [buffer.width as f32 * SCALE, buffer.height as f32 * SCALE],
+      );
+    },
+  );
 }
